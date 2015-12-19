2f5670ebad266f52119c005dc7e678ac5f43627f
Ralusama19 <ralucris.v@gmail.com>
2015-12-19 21:38:23 +0100

    Updated script

--- jutge.sh
+++ jutge.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status.
-
-problem=$1
-myPath=./$1
 file=$2
 myflags="-std=c++11 -Wall"
 g++ ${myflags} ${file} -o binary
 
 if [ "$#" -ge 3 ]; then
 	for (( i = 1; i <= $3; i++ )); do
-		./binary < ${1}/sample-${i}.inp > ${1}/sample-${i}.out
+		./binary < ${1}sample-${i}.inp > ${1}sample-${i}.out
 		echo Diferencies en joc de proves ${i}:
-		sdiff -s ${1}/sample-${i}.out ${1}/sample-${i}.cor
+		sdiff -s -E -a ${1}sample-${i}.out ${1}sample-${i}.cor
 	done
 else
-	./binary < ${1}/sample.inp > ${1}/sample.out
+	./binary < ${1}sample.inp > ${1}sample.out
 	echo Diferencies en el joc de proves:
-	sdiff -s ${1}/sample.out ${1}/sample.cor
+	sdiff -s -E -a ${1}sample.out ${1}sample.cor
 fi
 
 
