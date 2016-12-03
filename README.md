884cd245ca5189e3b19efdd84d972b2d72585385
ralusama19 <ralucris.v@gmail.com>
2016-12-03 18:21:32 +0100

    canviat ordre de parametres per a mes facilitat

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Script per compilar, provar i comparar tots els jocs de probes d'un problema de 
 (Descarregar i descomprimir les carpetes de jocs de proves DINS del directori del script)
 
 Sintaxi:
-./jutge.sh NOM-PROBLEMA NOM-ARXIU numero de jocs de proves
+./jutge.sh NOM-ARXIU NOM-PROBLEMA [numero de jocs de proves]
 
 
 (si el problema nomes te un joc de proves no s'ha d'utilitzar el tercer argument, altrament introduir el numero de jocs de proves que te el problema)
--- jutge.sh
+++ jutge.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-file=$2
+file=$1
 myflags="-std=c++11 -Wall"
 g++ ${myflags} ${file} -o binary
 
 if [ "$#" -ge 3 ]; then
 	for (( i = 1; i <= $3; i++ )); do
-		./binary < ${1}sample-${i}.inp > ${1}sample-${i}.out
+		./binary < ${2}sample-${i}.inp > ${2}sample-${i}.out
 		echo Diferencies en joc de proves ${i}:
-		sdiff -s -E -a ${1}sample-${i}.out ${1}sample-${i}.cor
+		sdiff -s -E -a ${2}sample-${i}.out ${2}sample-${i}.cor
 	done
 else
-	./binary < ${1}sample.inp > ${1}sample.out
+	./binary < ${2}sample.inp > ${2}sample.out
 	echo Diferencies en el joc de proves:
-	sdiff -s -E -a ${1}sample.out ${1}sample.cor
+	sdiff -s -E -a ${2}sample.out ${2}sample.cor
 fi
 
 
