351e5cad45b4260aef5923df8a58041f02c53fab
Ralu <ralucado@users.noreply.github.com>
2017-03-30 15:02:29 +0200

    new version, checks all test cases

--- jutge.sh
+++ jutge.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-
 file=$1
 myflags="-std=c++11 -Wall"
 g++ ${myflags} ${file} -o binary
 
-if [ "$#" -ge 3 ]; then
-	for (( i = 1; i <= $3; i++ )); do
-		./binary < ${2}sample-${i}.inp > ${2}sample-${i}.out
-		echo Diferencies en joc de proves ${i}:
-		sdiff -s -E -a ${2}sample-${i}.out ${2}sample-${i}.cor
-	done
-else
-	./binary < ${2}sample.inp > ${2}sample.out
-	echo Diferencies en el joc de proves:
-	sdiff -s -E -a ${2}sample.out ${2}sample.cor
-fi
-
-
+for f in ${2}*.inp; do
+        echo "processing ${f:0:-4} file"
+        ./binary < ${f} > ${f:0:-4}.out
+        echo Diferencies en joc de proves $f:
+        sdiff -s -E -a ${f:0:-4}.out ${f:0:-4}.cor #| wc -l
+        #diff ${f:0:-4}.out ${f:0:-4}.cor
+        rm ${f:0:-4}.out
+done
+rm binary
